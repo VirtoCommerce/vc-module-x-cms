91a56fe3586225f95b452e43261a2783e4368e27
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2024-06-17 15:56:32 +0200

    feat: add XCMS module

---
+++ src/VirtoCommerce.XCMS.Data/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+using GraphQL.Server;
+using Microsoft.Extensions.DependencyInjection;
+using VirtoCommerce.Xapi.Core.Extensions;
+using VirtoCommerce.XCMS.Core;
+
+namespace VirtoCommerce.XCMS.Data.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddXCMS(this IServiceCollection services, IGraphQLBuilder graphQlbuilder)
+        {
+            graphQlbuilder.AddSchema(typeof(CoreAssemblyMarker), typeof(DataAssemblyMarker));
+
+            return services;
+        }
+    }
+}
