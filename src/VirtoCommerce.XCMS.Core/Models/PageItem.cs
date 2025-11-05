b4d103b83f7a1f23a585efda958baa169c71ee40
Basil Kotov <basilkot@gmail.com>
2025-11-05 11:00:21 +0000

    remove organizationId from pageItem model

--- src/VirtoCommerce.XCMS.Core/Models/PageItem.cs
+++ src/VirtoCommerce.XCMS.Core/Models/PageItem.cs
@@ -6,7 +6,6 @@ namespace VirtoCommerce.XCMS.Core.Models
         public string Name { get; set; }
         public string Permalink { get; set; }
         public string RelativeUrl { get; set; }
-        public string OrganizationId { get; set; }
         public string Content { get; set; }
     }
 }
