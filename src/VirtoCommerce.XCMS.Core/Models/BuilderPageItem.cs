755faf6b14270688a7f0a07c8150a306c3e56349
Basil Kotov <basilkot@gmail.com>
2026-03-12 10:20:48 +0000

    VCST-4500: Preview static pages when VirtoPages module is disabled (#18)
    # Conflicts:
    #	src/VirtoCommerce.XCMS.Core/Schemas/BuilderPageItemType.cs
    #	src/VirtoCommerce.XCMS.Core/VirtoCommerce.XCMS.Core.csproj
    #	src/VirtoCommerce.XCMS.Data/Queries/GetPreviewBuilderPageQueryHandler.cs
    #	src/VirtoCommerce.XCMS.Web/module.manifest

---
+++ src/VirtoCommerce.XCMS.Core/Models/BuilderPageItem.cs
@@ -0,0 +1,9 @@
+namespace VirtoCommerce.XCMS.Core.Models;
+
+public class BuilderPageItem
+{
+    public string PageId { get; set; }
+    public string Permalink { get; set; }
+    public string Content { get; set; }
+
+}
